47bcebe295d2bb751b009a18c47abd07a87c8988
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-02-04 21:48:36 -0800

    Add index template.  - Create index.html  - Add mock data in index route definition  - Add for loop to display posts in index template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 # Microblog application routesself.
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'username': 'Elijah' }
+    posts = [
+        {
+            'author': { 'username': 'John' },
+            'body': 'This is a flask microblog.'
+        },
+        {
+            'author': { 'username': 'Eli' },
+            'body': 'This is definitely a flask microblog.'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
